3f2ef5b3c023bcc53b2cc8ed4e637ede5413d0c7
ka_res <kares.inf@gmail.com>
2021-03-11 18:59:54 +0100

    initial commit

---
+++ GuitarBoxDesigner.NET.Singleton/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GuitarBoxDesigner.NET.Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
